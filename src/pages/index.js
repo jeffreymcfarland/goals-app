f386af0d0b19bc37b3b6174d1a7845f6a2b89a08
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2022-06-01 13:25:17 -0500

    feat: index page responsive layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,13 +11,13 @@ const IndexPage = () => {
     return null;
   } else {
     return (
-      <div className="h-screen grid grid-cols-2">
-        <div className="flex shadow-2xl z-10">
-          <div className="m-auto text-center px-3">
+      <div className="h-screen grid grid-cols-1 md:grid-cols-2">
+        <div className="bg-transparent flex absolute w-screen h-screen z-10 md:w-auto md:bg-white md:relative md:shadow-2xl">
+          <div className="m-auto text-center p-3 m-auto">
             <h1 className="font-semibold text-6xl mb-4 text-slate-800">
               My Goals
             </h1>
-            <p className="text-xl max-w-md mb-4">
+            <p className="text-xl max-w-md mb-4 text-slate-800">
               Welcome to the perfect place to set your goals and track your
               progress. The best goals are the ones we continously work towards
               to acheive.
@@ -27,8 +27,8 @@ const IndexPage = () => {
                 Get Started
               </Button>
               <Button
-                appearance="subtle"
                 element={Link}
+                appearance="subtle"
                 to="/login"
                 state={{ newUser: false }}
               >
@@ -38,9 +38,9 @@ const IndexPage = () => {
           </div>
         </div>
         <StaticImage
-          className="object-cover"
+          className="h-screen opacity-50 md:opacity-80"
           src="../images/cool-background.png"
-          alt=""
+          alt="abstract vector art"
         />
       </div>
     );
