03fe52bdbff22b19264dd93e366e3f1e94d965df
Jeffrey McFarland <jeffrey.mcfarland@ramseysolutions.com>
2022-06-01 00:36:18 -0500

    index page responsive layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,44 +3,45 @@ import { isLoggedIn } from "../services/auth";
 import { Link, navigate } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 
-import Button from "../components/button";
-
 const IndexPage = () => {
   if (isLoggedIn()) {
     navigate("/dashboard");
     return null;
   } else {
     return (
-      <div className="h-screen grid grid-cols-2">
+      <div className="h-screen grid grid-cols-1 md:grid-cols-2">
         <div className="flex shadow-2xl z-10">
-          <div className="m-auto text-center px-3">
+          <div className="m-auto text-center p-3">
             <h1 className="font-semibold text-6xl mb-4 text-slate-800">
               My Goals
             </h1>
-            <p className="text-xl max-w-md mb-4">
+            <p className="text-xl max-w-md mb-4 text-slate-800">
               Welcome to the perfect place to set your goals and track your
               progress. The best goals are the ones we continously work towards
               to acheive.
             </p>
             <div className="flex justify-center gap-3">
-              <Button element={Link} to="/login" state={{ newUser: true }}>
+              <Link
+                to="/login"
+                state={{ newUser: true }}
+                className="px-4 py-2 rounded-md text-white bg-slate-500 hover:bg-slate-600"
+              >
                 Get Started
-              </Button>
-              <Button
-                appearance="subtle"
-                element={Link}
+              </Link>
+              <Link
                 to="/login"
                 state={{ newUser: false }}
+                className="px-4 py-2 rounded-md text-slate-500 bg-white hover:bg-slate-100"
               >
                 Sign In
-              </Button>
+              </Link>
             </div>
           </div>
         </div>
         <StaticImage
-          className="object-cover"
+          className="absolute md:relative opacity-50 md:opacity-80"
           src="../images/cool-background.png"
-          alt=""
+          alt="abstract vector art"
         />
       </div>
     );
